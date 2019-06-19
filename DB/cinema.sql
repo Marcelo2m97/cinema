55b684f80246297c9e3f6501a7831658c343a71b
Marcelo2m97 <marcelo2m1097@gmail.com>
2019-06-19 00:30:21 -0600

    Sample data added and code was reorder for it to run.

--- DB/cinema.sql
+++ DB/cinema.sql
@@ -17,7 +17,7 @@ CREATE TABLE USUARIO(
 	c_estado INT,
 	c_ciudad INT,
 	c_rol INT,
-	u_nombres VARCHAR(100),
+	u_nombre VARCHAR(100),
 	u_apellido VARCHAR(100),
 	u_fechaNacimiento DATE,
 	u_direccion TEXT,
@@ -86,6 +86,42 @@ CREATE TABLE PELICULA(
 	pelicula_creacion DATE,
 	pelicula_ultimaModificacion DATE
 );
+
+
+INSERT INTO PAIS VALUES (1, 'Estados Unidos');
+INSERT INTO PAIS VALUES (2, 'Canada');
+INSERT INTO PAIS VALUES (3, 'El Salvador');
+
+INSERT INTO ESTADO VALUES (1, 'California');
+INSERT INTO ESTADO VALUES (2, 'Florida');
+INSERT INTO ESTADO VALUES (3, 'Quebec');
+INSERT INTO ESTADO VALUES (4, 'Alberta');
+INSERT INTO ESTADO VALUES (5, 'San Salvador');
+INSERT INTO ESTADO VALUES (5, 'Santa Ana');
+
+INSERT INTO CIUDAD VALUES (1, 'Sacramento');
+INSERT INTO CIUDAD VALUES (2, 'San Francisco');
+INSERT INTO CIUDAD VALUES (3, 'Miami');
+INSERT INTO CIUDAD VALUES (4, 'Orlando');
+INSERT INTO CIUDAD VALUES (5, 'Quebec');
+INSERT INTO CIUDAD VALUES (6, 'Motreal');
+INSERT INTO CIUDAD VALUES (7, 'Edson');
+INSERT INTO CIUDAD VALUES (8, 'Calgary');
+INSERT INTO CIUDAD VALUES (9, 'San Salvador');
+INSERT INTO CIUDAD VALUES (10, 'Ayutuxtepeque');
+INSERT INTO CIUDAD VALUES (11, 'El Congo');
+INSERT INTO CIUDAD VALUES (12, 'Santa Ana');
+
+INSERT INTO ROL VALUES (1, 'Administrador');
+INSERT INTO ROL VALUES (2, 'Cliente');
+
+INSERT INTO USUARIO VALUES (1,1,1,1,1, 'George', 'Smith', '26/1/1987', 'direccion1', 'georges1', '12345678', true,false,$200.00);
+INSERT INTO USUARIO VALUES (2,1,1,2,2, 'James', 'Swift', '2/2/1989', 'direccion2', 'swift89', '12345678', false,false,$5.00);
+INSERT INTO USUARIO VALUES (3,1,2,3,1, 'Thomas', 'Johnson', '10/2/1980', 'direccion3', 'tj1002', '12345678', true,false,$500.00);
+INSERT INTO USUARIO VALUES (4,1,2,4,2, 'John', 'Steward', '5/12/1996', 'direccion4', 'stewardj', '12345678', false,false,$2.00);
+INSERT INTO USUARIO VALUES (5,3,5,9,2, 'Rodrigo', 'Alvarenga', '23/4/1998', 'direccion5', 'tupic_toxic', '12345678', true,false,$1.00);
+INSERT INTO USUARIO VALUES (5,3,6,12,2, 'Marcelo', 'Martinez', '26/10/1997', 'direccion6', 'marcelo2m97', '12345678', true,false,$20.00);
+INSERT INTO USUARIO VALUES (5,3,6,12,2, 'Edward', 'Martinez', '5/9/1997', 'direccion7', 'emartinezs', '12345678', false,false,$2000.00);
 
 --FK's
 ALTER TABLE	USUARIO
